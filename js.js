a6dfd921e147a0261a2cc248b313c6efd5dcfe7c
Strukov20 <32924439+Strukov20@users.noreply.github.com>
2020-02-18 17:28:28 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,46 @@
+'use srict';
+
+
+/*var msg = "Hello World";
+var matrix = "Approve!";
+console.log(msg);
+console.log(matrix);
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+console.log(second);
+console.log(pi);
+
+
+var number = 5;
+var string = "Ahoj";
+var sym = Symbol();
+var boolean = false;
+null;
+undefined;
+
+var obj = {};
+
+let something;
+console.log(something);*/
+
+let person = {
+
+	name: "John",
+	age: 32,
+	isMarried: true
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+//alert("Hello World!");
+let answer = prompt("Are you 18?", "Yes");
+console.log(typeof(arr));
